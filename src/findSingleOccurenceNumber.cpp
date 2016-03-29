8725e9a1dfa442b9829aa0df9df00278ca4df7e3
RajsekharKommala <RajsekharKommala@missionrnd.com>
2016-03-29 16:25:49 -0700

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -14,7 +14,24 @@ NOTES:
 There are better ways of solving the problem than a brute-force solution which is of O(n^2)
 complexity .
 */
+#define NULL 0
 
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+	//checking for null input
+	if (A == NULL)
+		return -1;
+	int arr_i, arr_j;
+	int arr_ele;
+	//checking for single occurence;;
+	for (arr_i = 0; arr_i < len; arr_i++) {
+		for (arr_j = 0; arr_j < len - 1; arr_j++)
+			if (A[arr_i] == A[arr_j] && arr_i != arr_j)
+				break;
+		if (arr_j == len - 1)
+			arr_ele = A[arr_i];
+	}
+	return arr_ele;
+
+
+	
 }
--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -22,6 +22,154 @@ struct transaction {
 	char description[20];
 };
 
+int who_isOlder(char *dob_1, char *dob_2);
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	//for null input;;
+	if (A == NULL || B== NULL)
+		return NULL;
+	struct transaction *result_trans_output;
+	//memory allocation for result operation;;
+	result_trans_output = (struct transaction *)malloc(sizeof(struct transaction)*(ALen + BLen));
+	// initializing the values;;;
+	int tran_i = 0, tran_j = 0, tran_k = 0;
+	while (tran_i < ALen && tran_j < BLen){//checking with both arrays lengths
+		if (who_isOlder(A[tran_i].date, B[tran_j].date) == 2)
+		{
+		   result_trans_output[tran_k].amount = B[tran_j].amount;
+			tran_j++;
+		}
+		else
+		{
+			result_trans_output[tran_k].amount = A[tran_i].amount;
+			tran_i++;
+		}
+		tran_k++;
+	}
+	while (tran_i<ALen){
+		result_trans_output[tran_k].amount = A[tran_i].amount;
+		tran_i++;
+		tran_k++;
+	}
+
+	while (tran_j<BLen){
+		result_trans_output[tran_k].amount = B[tran_j].amount;
+		tran_j++;
+		tran_k++;
+	}
+	return result_trans_output;
+}
+int who_isOlder(char *dob_1, char *dob_2) {
+	int i = 0, dd1 = 0, mm1 = 0, yyyy1 = 0, dd2 = 0, mm2 = 0, yyyy2 = 0;
+	while (dob_1[i] != '-'&&dob_1[i] != '\0')
+	{
+		if (dob_1[i] < '0' || dob_1[i] > '9')
+			return -1;
+		dd1 *= 10;
+		dd1 += dob_1[i] - '0';
+		i++;
+	}
+	if (dob_1[i] == '\0')
+		return -1;
+	i++;
+
+	while (dob_1[i] != '-'&&dob_1[i] != '\0')
+	{
+		if (dob_1[i] < '0' || dob_1[i] > '9')
+			return -1;
+		mm1 *= 10;
+		mm1 += dob_1[i] - '0';
+		i++;
+	}
+
+	if (dob_1[i] == '\0')
+		return -1;
+	i++;
+
+	while (dob_1[i] != '-'&&dob_1[i] != '\0')
+	{
+		if (dob_1[i] < '0' || dob_1[i] > '9')
+			return -1;
+		yyyy1 *= 10;
+		yyyy1 += dob_1[i] - '0';
+		i++;
+	}
+
+	if (dob_1[i] == '\0')
+	{
+		if (yyyy1 % 4 == 0 && mm1 == 2 && dd1 >= 30)
+			return -1;
+		if (yyyy1 % 4 != 0 && mm1 == 2 && dd1 >= 29)
+			return -1;
+		if (mm1 > 12 || dd1 > 31)
+			return -1;
+	}
+	else
+		return -1;
+
+	i = 0;
+
+	while (dob_2[i] != '-'&&dob_2[i] != '\0')
+	{
+		if (dob_2[i] < '0' || dob_2[i] > '9')
+			return -1;
+		dd2 *= 10;
+		dd2 += dob_2[i] - '0';
+		i++;
+	}
+	if (dob_2[i] == '\0')
+		return -1;
+	i++;
+	while (dob_2[i] != '-'&&dob_2[i] != '\0')
+	{
+		if (dob_2[i] < '0' || dob_2[i] > '9')
+			return -1;
+		mm2 *= 10;
+		mm2 += dob_2[i] - '0';
+
+		i++;
+	}
+	if (dob_2[i] == '\0')
+		return -1;
+	i++;
+	while (dob_2[i] != '-'&&dob_2[i] != '\0')
+	{
+		if (dob_2[i] < '0' || dob_2[i] > '9')
+			return -1;
+		yyyy2 *= 10;
+		yyyy2 += dob_2[i] - '0';
+		i++;
+	}
+	if (dob_2[i] == '\0')
+	{
+		if (yyyy2 % 4 == 0 && mm2 == 2 && dd2 >= 30)
+			return -1;
+		if (yyyy2 % 4 != 0 && mm2 == 2 && dd2 >= 29)
+			return -1;
+		if (mm2 > 12 || dd2 > 31)
+			return -1;
+	}
+	else
+		return -1;
+
+	if (yyyy1>yyyy2)
+		return 2;
+	else if (yyyy1<yyyy2)
+		return 1;
+	else
+	{
+		if (mm1>mm2)
+			return 2;
+		else if (mm1<mm2)
+			return 1;
+		else
+		{
+			if (dd1>dd2)
+				return 2;
+			else if (dd1<dd2)
+				return 1;
+			else
+				return 0;
+		}
+	}
+	return 0;
 }
